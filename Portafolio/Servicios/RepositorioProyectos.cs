a3939bb50af107c6b8e44c90fecb5ba0ba2540b5
AliciaEA <aliestrada@uamv.edu.ni>
2025-01-08 12:19:31 -0600

    Respetando el Principio de responsabilidad Unica. Inyeccion de dependencias. Vistas Parciales

---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,32 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public class RepositorioProyectos
+    {
+        public List<ProyectoDTO> ObtenerProyectos()
+        {
+            return new List<ProyectoDTO> {
+                new ProyectoDTO {
+                    Titulo = "Amazon",
+                    Descripcion = "Descripción del proyecto 1",
+                    Link = "https://www.amazon.com/-/es/",
+                    ImagenURL = "/images/Amazon.png"
+
+                },
+                new ProyectoDTO {
+                    Titulo = "Youtube",
+                    Descripcion = "Descripción del proyecto 2",
+                    Link = "https://www.youtube.com/",
+                    ImagenURL = "/images/Youtube.png"
+                },
+                new ProyectoDTO {
+                    Titulo = "CSS",
+                    Descripcion = "Descripción del proyecto 3",
+                    Link = "https://www.slayingthedragon.io/dashboard/css/introduction",
+                    ImagenURL = "/images/CSS.png"
+                }
+            };
+        }
+    }
+}
